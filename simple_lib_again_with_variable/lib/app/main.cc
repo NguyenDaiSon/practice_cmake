1df82504bb868552db431b34237b076e1765c1e6
Nguyen Dai Son <daison.nguyen@rapsodo.com>
2022-12-02 11:22:03 +0800

    Delete app nested in lib

--- simple_lib_again_with_variable/lib/app/main.cc
+++
@@ -1,8 +0,0 @@
-#include "my_lib.h"
-// clang-format -i -style=WebKit *.cc *.h
-// Reference: https://stackoverflow.com/questions/28896909/how-to-call-clang-format-over-a-cpp-project-folder
-
-int main()
-{
-    greeting();
-}
