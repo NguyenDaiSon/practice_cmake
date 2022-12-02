e402d8926cc95943e6f0defe086fe9ef2d8fe70b
Nguyen Dai Son <daison.nguyen@rapsodo.com>
2022-12-02 11:15:11 +0800

    This program is to learn basic about CMake-options

---
+++ simple_lib_again_with_option/lib/CMakeFiles/SimpleLibAgainWithVariable.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SimpleLibAgainWithVariable.dir/my_lib.cc.o"
+  "libSimpleLibAgainWithVariable.a"
+  "libSimpleLibAgainWithVariable.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SimpleLibAgainWithVariable.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ simple_lib_again_with_option/test/CMakeFiles/test1.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/daison/practice_cmake/simple_lib_again_with_option/test/test1.cc" "/home/daison/practice_cmake/simple_lib_again_with_option/test/CMakeFiles/test1.dir/test1.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "lib/."
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/daison/practice_cmake/simple_lib_again_with_option/lib/CMakeFiles/SimpleLibAgainWithVariable.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
